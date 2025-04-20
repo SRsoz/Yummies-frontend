c374dd5521dc742830386619164955ac4b2bd98b
SRsoz <sozyar.rauf@chasacademy.se>
2025-04-20 21:27:11 +0200

    first real commit

---
+++ vanilla-setup/vite.config.ts
@@ -0,0 +1,22 @@
+import { register } from 'node:module';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        signIn: resolve(__dirname, 'signIn.html'),
+        register: resolve(__dirname, 'register.html'),
+        yourRecipes: resolve(__dirname, 'yourRecipes.html'),
+        createRecipe: resolve(__dirname, 'createRecipe.html'),
+        yourAccount: resolve(__dirname, 'yourAccount.html'),
+        updateRecipe: resolve(__dirname, 'updateRecipe.html'),
+      },
+    },
+  },
+});
